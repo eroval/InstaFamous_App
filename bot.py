b2020fce7d475fb1aae8285f393299198ea1ba06
Denis Zangarov <gamingdeathadder98@gmail.com>
2023-06-25 16:43:24 +0300

    add people which i actually follow

---
+++ bot.py
@@ -0,0 +1,6 @@
+people_i_actually_follow=["bellahadid","ralitsa._.s","emchoou","krisgeorgievv","tedtoefoe","alex_gogosheva","dragomir_asamov",
+"georgizayakov", "ppatanasov98", "spasivanov98", "__mitranov__", "ympetkov", "z_madzharov", "techo.k", "killtheliver", "iliyanstratiev99",
+"rali.zheleva"]
+
+
+print(people_i_actually_follow)
